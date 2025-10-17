7e01d3c0b8dd67c4677aff1609ff120357e759ee
Subbarao Bellamkonda <oar_abbus@yahoo.com>
2025-10-17 12:57:00 -0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 def main():
     return '''
      <form action="/echo_user_input" method="POST">
-         <input name="user_input">
+         <label for="user_input">County:</label>
+         <input type="text" id="user_input" name="user_input">
          <input type="submit" value="Submit!">
      </form>
      '''
@@ -16,4 +17,18 @@ def main():
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
     input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+    data = get_incentives_db(input_text)
+    return "Incentives for " + input_text + " County: " + data["Name"]
+
+
+def get_incentives_db(name):
+    data = {
+    "Name": "Alameda Municipal Power - Electric Vehicle Rebate Program",
+    "State": "CA",
+    "Category": "Financial Incentive",
+    "IncentiveType": "Rebate Program",
+    "Created": "07/02/2021",
+    "Updated": "01/28/2025" 
+    }
+    return data
+
